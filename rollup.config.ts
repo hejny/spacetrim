caa4cce8f0a841d945d2162cfdf9e81ed413ba94
Pavol Hejny <me@pavolhejny.com>
2021-11-09 15:46:50 +0100

    Misc configurations

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,10 +3,12 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
     // !!! configPlugin: 'typescript',
-    input: './src/main.ts',
+    input: './src/spaceTrim.ts',
     output: {
-      file: './dist/bundle.js',
-      format: 'cjs'
+      file: './dist/spaceTrim.js',
+      //format: 'cjs'
+      format: 'iife',
+      sourcemap: true,
     },
     plugins: [typescript({lib: ["esnext", "dom"], target: "es5"})]
   };
