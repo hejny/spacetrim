4b08d7b5520ee35c604c1d6eef3d762377e1781e
Pavol Hejny <me@pavolhejny.com>
2021-11-13 16:43:37 +0100

    Beter organization of actions and processes

--- src/spaceTrim.ts
+++ src/spaceTrim.ts
@@ -1,22 +1,38 @@
 export default spaceTrim;
 
-export function spaceTrim(str: string /* TODO: , char: RegExp = /\s/*/): string {
+export function spaceTrim(
+    str: string /* TODO: , char: RegExp = /\s/*/,
+): string {
     // ✂️ Trimming from top and bottom
     str = spaceTrimFromTop(str);
-    str = spaceTrimFromTop(str.split('\n').reverse().join('\n')).split('\n').reverse().join('\n');
+    str = spaceTrimFromTop(str.split('\n').reverse().join('\n'))
+        .split('\n')
+        .reverse()
+        .join('\n');
 
     // ✂️ Trimming from left and right
     let minStartWhitespaceLength: null | number = null;
     let maxContentLength: number = 0;
     const lines = str.split('\n');
-    for (const line of lines) {
-        const { startWhitespace, content } = line.match(/^(?<startWhitespace>\s*)(?<content>.*?)(\s*)$/)!.groups as any;
 
-        minStartWhitespaceLength = Math.min(minStartWhitespaceLength || 99999999999999, startWhitespace.length);
+    for (const line of lines) {
+        const { startWhitespace, content } = line.match(
+            /^(?<startWhitespace>\s*)(?<content>.*?)(\s*)$/,
+        )!.groups as any;
+
+        minStartWhitespaceLength = Math.min(
+            minStartWhitespaceLength || 99999999999999,
+            startWhitespace.length,
+        );
         maxContentLength = Math.max(maxContentLength, content.length);
     }
     str = lines
-        .map((line) => line.substring(minStartWhitespaceLength!, minStartWhitespaceLength! + maxContentLength))
+        .map((line) =>
+            line.substring(
+                minStartWhitespaceLength!,
+                minStartWhitespaceLength! + maxContentLength,
+            ),
+        )
         .join('\n');
 
     return str;
